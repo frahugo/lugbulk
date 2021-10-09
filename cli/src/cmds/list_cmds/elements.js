a8b19e88e2fdaa884fec93c0f463f785832b4256
Hugo Frappier <hugo@cekoya.com>
2021-10-09 12:21:04 -0400

    Add library as a dependency

--- cli/src/cmds/list_cmds/elements.js
+++ cli/src/cmds/list_cmds/elements.js
@@ -1,7 +1,14 @@
+// const { Element } = require("../../../../lib/element.js");
+const { Element } = require("lugbulk-lib/element");
+
 exports.command = "elements";
 exports.desc = "List elements from a LugBulk order";
 exports.builder = {};
 exports.handler = function (argv) {
+  const element = new Element("123456", "BRICK 1 X 1", "black");
+
+  console.log(element);
+
   if (argv.output === undefined) {
     console.log("Listing elements from %s", argv.input);
   } else {
