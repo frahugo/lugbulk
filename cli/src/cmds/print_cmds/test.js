8d04d0553e87795983e81be7124f9f9d64c44de1
Hugo Frappier <hugo@cekoya.com>
2021-10-11 17:30:49 -0400

    Fix filenames

--- cli/src/cmds/print_cmds/test.js
+++ cli/src/cmds/print_cmds/test.js
@@ -1,17 +1,12 @@
-const chalk = require("chalk");
-const TextTable = require("text-table");
-const Papa = require("papaparse");
 const fs = require("fs");
 const path = require("path");
 const Dymo = require("dymojs");
-const readlineSync = require("readline-sync");
-const pluralize = require("pluralize");
 
 const { Element } = require("lugbulk-lib/src/element");
 const { Order } = require("lugbulk-lib/src/order");
 
-exports.command = "elements";
-exports.desc = "Print elements from a LugBulk order";
+exports.command = "test";
+exports.desc = "Print a test from a LugBulk order";
 exports.builder = {};
 exports.handler = function (argv) {
   const order = new Order();
@@ -29,32 +24,13 @@ async function print(order) {
     lotLabelXml: fs.readFileSync(lotLabelFile, "utf8"),
   };
 
-  console.log("%d elements to print.", order.elements.length);
+  element = order.elements[0];
 
-  // Note: need recursion so the promises to work sequentially with the prompts.
-  processElement(0, order.elements, labelConfig);
-}
-
-function processElement(index, elements, labelConfig) {
-  if (index + 1 > elements.length) {
-    return;
-  }
-  element = elements[index];
-  var answer = readlineSync.keyIn(`Print element ${element.id} (y/n/q)? `, { limit: "$<ynq>" });
-  switch (answer) {
-    case "y":
-      printElement(element, labelConfig).then((result) => {
-        printLot(element, labelConfig).then((result) => {
-          processElement(++index, elements, labelConfig);
-        });
-      });
-      break;
-    case "n":
-      processElement(++index, elements, labelConfig);
-      break;
-    case "q":
-      break;
-  }
+  printElement(element, labelConfig).then((result) => {
+    printLot(element, labelConfig).then((result) => {
+      console.log("2 labels should have been printed.");
+    });
+  });
 }
 
 function printElement(element, labelConfig) {
@@ -74,10 +50,8 @@ function printLot(element, labelConfig) {
   const labelParts = [];
 
   labelParts.push("<LabelSet>");
-  for (var lot of element.lots) {
-    recordXml = buildRecordXml(element, lot);
-    labelParts.push(recordXml);
-  }
+  recordXml = buildRecordXml(element, element.lots[0]);
+  labelParts.push(recordXml);
   labelParts.push("</LabelSet>");
 
   let labelSetXml = labelParts.join("");
@@ -86,8 +60,6 @@ function printLot(element, labelConfig) {
 }
 
 function buildRecordXml(element, lot) {
-  var totalLots = pluralize("lot", element.lots.length, true);
-
   return `<LabelRecord>
         <ObjectData Name="ELEMENT_ID">${element.id}</ObjectData>
         <ObjectData Name="ELEMENT_NAME">${element.name}</ObjectData>
@@ -95,7 +67,7 @@ function buildRecordXml(element, lot) {
         <ObjectData Name="PSEUDO">${lot.pseudo}</ObjectData>
         <ObjectData Name="SEQUENCE">${lot.sequence}</ObjectData>
         <ObjectData Name="QUANTITY">${lot.quantity}</ObjectData>
-        <ObjectData Name="TOTAL">${totalLots}</ObjectData>
+        <ObjectData Name="TOTAL">1 lot</ObjectData>
     </LabelRecord>`;
 }
 
