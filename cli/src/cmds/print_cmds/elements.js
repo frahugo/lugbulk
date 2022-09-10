31b29a9175cbead96db311fe0a377797343deeaf
Hugo Frappier <hugo@cekoya.com>
2022-09-10 10:09:06 -0400

    Print member ID on label

--- cli/src/cmds/print_cmds/elements.js
+++ cli/src/cmds/print_cmds/elements.js
@@ -35,14 +35,14 @@ async function print(order) {
   console.log("%d labels to print.", totalCount);
 
   // Note: need recursion so the promises to work sequentially with the prompts.
-  processElement(0, order.elements, labelConfig, 0);
+  processElement(0, order, labelConfig, 0);
 }
 
-function processElement(index, elements, labelConfig, runningCount) {
-  if (index + 1 > elements.length) {
+function processElement(index, order, labelConfig, runningCount) {
+  if (index + 1 > order.elements.length) {
     return;
   }
-  element = elements[index];
+  element = order.elements[index];
   count = element.lots.length;
   var answer = readlineSync.keyIn(
     `Print element ${element.id} - ${count} labels, ${runningCount} so far (y/n, [q]uit [r]eset [g]oto)? `,
@@ -52,25 +52,25 @@ function processElement(index, elements, labelConfig, runningCount) {
   );
   switch (answer) {
     case "y":
-      printElementLots(element, labelConfig).then((result) => {
+      printElementLots(element, order, labelConfig).then((result) => {
         console.log("%d lots printed.", element.lots.length);
-        processElement(++index, elements, labelConfig, runningCount + count);
+        processElement(++index, order, labelConfig, runningCount + count);
       });
       break;
     case "n":
-      processElement(++index, elements, labelConfig, runningCount);
+      processElement(++index, order, labelConfig, runningCount);
       break;
     case "r":
-      processElement(++index, elements, labelConfig, 0);
+      processElement(++index, order, labelConfig, 0);
       break;
     case "g":
       var elementId = readlineSync.question("Element ID: ");
-      var elementIndex = elements.findIndex((element) => element.id == elementId);
+      var elementIndex = order.elements.findIndex((element) => element.id == elementId);
       if (elementIndex == -1) {
         console.log("Element not found.");
-        processElement(index, elements, labelConfig, runningCount);
+        processElement(index, order, labelConfig, runningCount);
       } else {
-        processElement(elementIndex, elements, labelConfig, runningCount);
+        processElement(elementIndex, order, labelConfig, runningCount);
       }
       break;
     case "q":
@@ -78,25 +78,27 @@ function processElement(index, elements, labelConfig, runningCount) {
   }
 }
 
-function printElementLots(element, labelConfig) {
+function printElementLots(element, order, labelConfig) {
   var firstLot = element.lots[0];
   var otherLots = element.lots.slice(1);
 
   if (otherLots.length > 0) {
-    return printLots([firstLot], labelConfig.dymo, labelConfig.lotFirstLabelXml).then((result) => {
-      return printLots(otherLots, labelConfig.dymo, labelConfig.lotLabelXml);
-    });
+    return printLots([firstLot], order, labelConfig.dymo, labelConfig.lotFirstLabelXml).then(
+      (result) => {
+        return printLots(otherLots, order, labelConfig.dymo, labelConfig.lotLabelXml);
+      }
+    );
   } else {
-    return printLots([firstLot], labelConfig.dymo, labelConfig.lotFirstLabelXml);
+    return printLots([firstLot], order, labelConfig.dymo, labelConfig.lotFirstLabelXml);
   }
 }
 
-function printLots(lots, dymo, labelXml) {
+function printLots(lots, order, dymo, labelXml) {
   const labelParts = [];
 
   labelParts.push("<LabelSet>");
   for (var lot of lots) {
-    recordXml = buildRecordXml(element, lot);
+    recordXml = buildRecordXml(element, lot, order);
     labelParts.push(recordXml);
   }
   labelParts.push("</LabelSet>");
@@ -106,14 +108,18 @@ function printLots(lots, dymo, labelXml) {
   return printLabels(dymo, labelXml, labelSetXml);
 }
 
-function buildRecordXml(element, lot) {
+// 4211075
+
+function buildRecordXml(element, lot, order) {
   var totalLots = pluralize("lot", element.lots.length, true);
+
+  var buyer = order.findBuyer(lot.pseudo);
 
   return `<LabelRecord>
         <ObjectData Name="ELEMENT_ID">${element.id} (${element.sequenceNumber})</ObjectData>
         <ObjectData Name="ELEMENT_NAME">${element.name}</ObjectData>
         <ObjectData Name="ELEMENT_COLOR">${element.color}</ObjectData>
-        <ObjectData Name="PSEUDO">${lot.pseudo}</ObjectData>
+        <ObjectData Name="PSEUDO">${lot.pseudo} (${buyer.memberId})</ObjectData>
         <ObjectData Name="SEQUENCE">${lot.sequence}</ObjectData>
         <ObjectData Name="QUANTITY">${lot.quantity}</ObjectData>
         <ObjectData Name="TOTAL">${totalLots}</ObjectData>
