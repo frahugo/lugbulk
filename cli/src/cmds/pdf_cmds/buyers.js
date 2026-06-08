1528cab06ad097cafeb532718dc528fac9629c10
Hugo Frappier <hugo@cekoya.com>
2026-06-08 18:19:31 -0400

    chore: Remove Club.js as it is no longer used in the project

--- cli/src/cmds/pdf_cmds/buyers.js
+++ cli/src/cmds/pdf_cmds/buyers.js
@@ -5,22 +5,19 @@ const pluralize = require("pluralize");
 const Handlebars = require("handlebars");
 
 const { Order } = require("lugbulk-lib/src/order");
-const { Club } = require("lugbulk-lib/src/club");
 
-exports.command = "buyers <order_csv_file> <members_csv_file>";
+exports.command = "buyers <order_csv_file>";
 exports.desc = "Generate PDF file for buyers";
 exports.builder = {};
 exports.handler = async function (argv) {
   const order = new Order();
-  const club = new Club();
   const output = argv.output === undefined ? "./buyers.pdf" : argv.output;
 
   await order.load(argv.order_csv_file);
-  club.load(argv.members_csv_file);
-  print(order, club, output);
+  print(order, output);
 };
 
-async function print(order, club, output) {
+async function print(order, output) {
   const template = path.resolve(path.join(__dirname, "../../../resources/buyers.html"));
 
   var html = fs.readFileSync(template, "utf8");
@@ -43,7 +40,7 @@ async function print(order, club, output) {
 
   var document = {
     html: html,
-    data: { order: order, club: club, count: order.buyers.length },
+    data: { order: order, count: order.buyers.length },
     path: output,
     type: "",
   };
@@ -57,14 +54,6 @@ async function print(order, club, output) {
       console.error(error);
     });
 }
-
-Handlebars.registerHelper("city", function (club, pseudo) {
-  return club.findMember(pseudo).city;
-});
-
-Handlebars.registerHelper("region", function (club, pseudo) {
-  return club.findMember(pseudo).region;
-});
 
 Handlebars.registerHelper("distribution_badge", function (method) {
   const labels = { self: "Sur place", region: "Région", person: "Via" };
